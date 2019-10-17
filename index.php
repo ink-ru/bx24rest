a6d68a6028e9122b7779101e3334a0430b8a871e
Васин Юрий <george.a.wise@gmail.com>
2019-10-17 09:12:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,44 +2,25 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Quick start. Local server-side application with UI</title>
+    <title>Quick start. Local static application</title>
 </head>
 <body>
-<div id="auth-data">OAuth 2.0 data from REQUEST:
-    <PRE>
-    <?
-        print_r($_REQUEST);
-    ?>
-    </PRE>
-</div>
-<div id="name">
+    <div id="name"></div>
 
-    <?
-        // use $_REQUEST data as OAuth 2.0 params to execute REST API calls
+    <script src="//api.bitrix24.com/api/v1/"></script>
+    <script>
+        // Make a call to REST when JS SDK is loaded
+        BX24.init(function(){
 
-        $queryUrl = 'https://'.$_REQUEST['DOMAIN'].'/rest/user.current.json';
+            BX24.callMethod('user.current', {}, function(res){
+                var name = document.getElementById('name');
+                name.innerHTML = res.data().NAME + ' ' + res.data().LAST_NAME;
+                console.log(res.data());
+            });
 
-        // as user.current does not have any specific parameters we just set an access_token ("auth")
-        $queryData = http_build_query(array(
-            "auth" => $_REQUEST['AUTH_ID']
-        ));
+            console.log('B24 SDK is ready!', BX24.isAdmin());
 
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_SSL_VERIFYPEER => 0,
-            CURLOPT_POST => 1,
-            CURLOPT_HEADER => 0,
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => $queryUrl,
-            CURLOPT_POSTFIELDS => $queryData,
-        ));
-
-        $result = json_decode(curl_exec($curl), true);
-        curl_close($curl);
-
-        // print_r($result);
-        echo $result['result']['NAME'].' '.$result['result']['LAST_NAME'];
-    ?>
-</div>
+        });
+    </script>
 </body>
 </html>
