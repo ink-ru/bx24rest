76577a3c037f5e999b2b50fefa201895cabfea5a
Васин Юрий <george.a.wise@gmail.com>
2019-10-17 09:20:55 +0300

    Update index.php

--- index.php
+++ index.php
@@ -39,6 +39,36 @@
 
         // print_r($result);
         echo $result['result']['NAME'].' '.$result['result']['LAST_NAME'];
+    
+        // ================================================================
+        // use $_REQUEST data as OAuth 2.0 params to execute REST API calls
+
+        filter[CLOSED]=Y&filter[STAGE_SEMANTIC_ID]=S&Params[NAV_PARAMS][nPageSize]=50&Params[NAV_PARAMS][iNumPage]=2
+        $queryUrl = 'https://'.$_REQUEST['DOMAIN'].'/rest/crm.deal.list.json';
+
+        // as user.current does not have any specific parameters we just set an access_token ("auth")
+        $queryData = http_build_query(array(
+            "auth" => $_REQUEST['AUTH_ID']
+        ));
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_POST => 1,
+            CURLOPT_HEADER => 0,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $queryUrl,
+            CURLOPT_POSTFIELDS => $queryData,
+        ));
+
+        $result = json_decode(curl_exec($curl), true);
+        curl_close($curl);
+        
+        echo '<h4>Deals</h4><pre>'
+        print_r($result);
+        echo '</pre>'
+        
+    
     ?>
 </div>
 </body>
